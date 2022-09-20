ba70e6032954f7af83679f7b15be3fd6ab46f277
Corentin ROZET <corentin.rozet@epitech.eu>
2022-09-20 12:10:37 +0200

    [ADD] sources + main.cpp: base code

---
+++ sources/main.cpp
@@ -0,0 +1,15 @@
+/*
+   Author: Corentin ROZET
+   Create Time: 2022-09-20 12:07:24
+   Modified by: Corentin ROZET
+   Modified time: 2022-09-20 12:10:08
+   Description: Hello World!
+*/
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
